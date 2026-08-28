9cd12c576280bec042e62719af50b0d0d20e6f91
GitGud6767 <prakarsaraka0@gmail.com>
2026-08-28 15:46:28 +0700

    passed gosec check

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"embed"
 	"io"
 	"log"
+	"strconv"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -96,8 +98,9 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
+	ReadHeaderTimeout: time.Second * 5,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving on port: %s\n", strconv.Qoute(port))
 	log.Fatal(srv.ListenAndServe())
 }
